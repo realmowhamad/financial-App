22df5ebface5d966a8b9a2fbefb91b095617836d
Mohammad Mirhoseini <Rm.mirhoseini@gmail.com>
2023-01-07 18:38:40 +0330

    start Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
+import { useRoutes } from 'react-router-dom';
 import './App.css';
+import Routes from './Router/Router';
+
 
 function App() {
+  const routes = useRoutes(Routes)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {routes}
     </div>
   );
 }
---
+++ src/Router/Router.js
@@ -0,0 +1,7 @@
+import MainPage from "../Pages/MainPage/MainPage";
+
+const Routes = [
+    { path: '/', element: <MainPage /> }
+]
+
+export default Routes
